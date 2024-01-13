9f7ef4300e899d5b145b9a6387242e09a382ec08
Houston4444 <picotmathieu@gmail.com>
2024-01-13 15:43:42 +0100

    adapt views menu in main context menu to the menu of the views selector widget

--- patchbay/views_menu.py
+++ patchbay/views_menu.py
@@ -24,76 +24,84 @@ class ViewsMenu(QMenu):
     def _build(self):
         self.clear()
         
-        view_keys = self.mng.views.keys()
-        n_views = len(self.mng.views)
+        view_keys = [n for n in self.mng.views.keys()]
         
-        if n_views > 1:
-            remove_menu = QMenu(
-                _translate('views_menu', 'Remove the view'), self)
-            remove_menu.setIcon(QIcon.fromTheme('document-close'))
-            
-            for view in view_keys:
-                view_data = self.mng.views_datas.get(view)
-                if view_data is None or not view_data.name:
-                    view_name = _translate('views_menu', 'View n°%i') % view
-                else:
-                    view_name = view_data.name
+        for view in view_keys:
+            view_data = self.mng.views_datas.get(view)
+            if view_data is None or not view_data.name:
+                view_name = _translate('views_menu', 'View n°%i') % view
+            else:
+                view_name = view_data.name
 
-                view_text = view_name
-                if 0 <= view <= 9:
-                    view_text += f'\tAlt+{view}'
+            view_text = view_name
+            if 0 <= view <= 9:
+                view_text += f'\tAlt+{view}'
 
-                view_act = self.addAction(view_text)
-                view_act.setData(view)
-                view_act.triggered.connect(self._change_view)                    
-                    
-                rm_act = remove_menu.addAction(view_name)
-                rm_act.setData(view)
-                rm_act.triggered.connect(self._remove_view)
-                
-                if view == self.mng.view_number:
-                    view_act.setEnabled(False)
-                    rm_act.setEnabled(False)
+            view_act = self.addAction(view_text)
+            view_act.setData(view)
+            view_act.triggered.connect(self._change_view)
 
-            self.addSeparator()
-            
-            self.addMenu(remove_menu)
-            self.addSeparator()
+            if view == self.mng.view_number:
+                view_act.setEnabled(False)
+
+        self.addSeparator()
         
-        new_view_act = self.addAction(
-            _translate('views_menu', 'New view'))
-        new_view_act.setIcon(QIcon.fromTheme('document-new'))
-        new_view_act.triggered.connect(self._new_view)
-        
-        if n_views > 1:
-            mv_view_act = self.addAction(
-                _translate('views_menu', 'Rename the current view'))
-            mv_view_act.setIcon(QIcon.fromTheme('edit-rename'))
-            mv_view_act.triggered.connect(self._rename_view)
-        
-        clear_view_act = self.addAction(
+        act_mv_view = self.addAction(
+            _translate('views_menu', 'Rename'))
+        act_mv_view.setIcon(QIcon.fromTheme('edit-rename'))
+        act_mv_view.triggered.connect(self._rename_view)
+
+        act_rm_view = self.addAction(
+            _translate('views_menu', 'Remove'))
+        act_rm_view.setIcon(QIcon.fromTheme('edit-delete'))
+        act_rm_view.triggered.connect(self._remove_view)
+
+        act_clear_absents = self.addAction(
             _translate('views_menu', 'Forget the positions of those absent'))
-        clear_view_act.setIcon(QIcon.fromTheme('edit-clear-all'))
-        clear_view_act.triggered.connect(self._clear_view)
-
-    @pyqtSlot()
-    def _new_view(self):
-        self.mng.new_view()
-    
-    @pyqtSlot()
-    def _rename_view(self):
-        new_name, ok = QInputDialog.getText(
-            self.mng.main_win,
-            _translate('views_menu', 'Rename view'),
-            _translate('views_menu', 'New view name :'))
+        act_clear_absents.setIcon(QIcon.fromTheme('edit-clear-all'))
+        act_clear_absents.triggered.connect(self._clear_absents)
         
-        if ok:
-            self.mng.rename_current_view(new_name)
-    
-    
-    @pyqtSlot()
-    def _clear_view(self):
-        print('clear viewww')
+        change_num_menu = QMenu(
+            _translate('views_menu', 'Change view number to...'), self)
+        
+        if self.mng is not None and view_keys:
+            n_nums_in_change_menu = max(max(view_keys) + 2, 10)
+        else:
+            n_nums_in_change_menu = 10
+        
+        for i in range(1, n_nums_in_change_menu):
+            act_new_view_num = change_num_menu.addAction(str(i))
+            if self.mng is not None:
+                if self.mng.views.get(i) is not None:
+                    if self.mng.views_datas.get(i) is not None:
+                        view_name = self.mng.views_datas[i].name
+                        act_new_view_num.setText(f'{i}\t{view_name}')
+
+                    if i == self.mng.view_number:
+                        act_new_view_num.setEnabled(False)
+                    else:
+                        act_new_view_num.setIcon(QIcon.fromTheme('reverse'))
+
+            act_new_view_num.setData(i)
+            act_new_view_num.triggered.connect(self._change_view_number)
+
+        self.addMenu(change_num_menu)
+
+        self.addSeparator()
+
+        act_remove_others = self.addAction(
+            QIcon.fromTheme('edit-delete'),
+            _translate('views_menu', 'Remove all other views'))
+        act_remove_others.triggered.connect(self._remove_all_other_views)
+
+        act_new_view = self.addAction(
+            _translate('views_menu', 'New view'))
+        act_new_view.setIcon(QIcon.fromTheme('document-new'))
+        act_new_view.triggered.connect(self._new_view)
+        
+        if len(view_keys) <= 1:
+            act_rm_view.setEnabled(False)
+            act_remove_others.setEnabled(False)
 
     @pyqtSlot()
     def _change_view(self):
@@ -101,12 +109,42 @@ class ViewsMenu(QMenu):
         self.mng.change_view(view_number)
 
     @pyqtSlot()
+    def _rename_view(self):
+        view_data = self.mng.views_datas.get(self.mng.view_number)
+        if view_data is None:
+            view_name = ''
+        else:
+            view_name = view_data.name
+        
+        new_name, ok = QInputDialog.getText(
+            self.mng.main_win,
+            _translate('views_menu', 'Rename view'),
+            _translate('views_menu', 'New view name :'),
+            text=view_name)
+        
+        if ok:
+            self.mng.rename_current_view(new_name)
+
+    @pyqtSlot()
+    def _clear_absents(self):
+        self.mng.clear_absents_in_view()
+
+    @pyqtSlot()
+    def _change_view_number(self):
+        new_num: int = self.sender().data()
+        self.mng.change_view_number(new_num)
+
+    @pyqtSlot()
+    def _remove_all_other_views(self):
+        self.mng.remove_all_other_views()
+
+    @pyqtSlot()
+    def _new_view(self):
+        self.mng.new_view()
+
+    @pyqtSlot()
     def _remove_view(self):
-        view_number: int = self.sender().data()
-        if view_number in self.mng.views.keys():
-            self.mng.views.pop(view_number)
-        if view_number in self.mng.views_datas.keys():
-            self.mng.views_datas.pop(view_number)
+        self.mng.remove_view()
     
     def showEvent(self, event) -> None:
         self._build()
