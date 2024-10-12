7f0bc4ce8c4b4892bd33e088bb377ba333587c7f
Houston4444 <picotmathieu@gmail.com>
2024-10-12 09:20:58 +0200

    adapt portgroups json rewrite

--- patchbay/patchcanvas/base_enums.py
+++ patchbay/patchcanvas/base_enums.py
@@ -12,34 +12,98 @@ def from_json_to_str(input_dict: dict[str, Any]) -> str:
     return a str containing the json contents with a 2 chars indentation
     and xy pos grouped on the same line.'''
 
+    PATH_OPENING = 0
+    PATH_IN = 1
+    PATH_CLOSING = 2
+
     json_str = json.dumps(input_dict, indent=2)
-
     final_str = ''
-    comp_line = ''
-
+    
+    path = list[str]()
+    path_step = PATH_IN
+    
     for line in json_str.splitlines():
-        if line.strip() == '"pos": [':
-            comp_line = line
-            continue
+        strip = line.strip()
         
-        if comp_line:
-            comp_line += line.strip()
-            if comp_line.endswith(','):
-                comp_line += ' '
+        if line.endswith(('{', '[')):
+            path_name = ''
+            if strip.startswith('"') and strip[:-1].endswith('": '):
+                path_name = strip[1:-4]
 
-            if line.strip().startswith(']'):
-                final_str += comp_line
+            n_spaces = 0
+            for c in line:
+                if c != ' ':
+                    break
+                n_spaces += 1
+            
+            path = path[:(n_spaces // 2)]
+            path.append(path_name)
+            path_step = PATH_OPENING
+        
+        elif line.endswith(('],', ']', '},', '}')):
+            path_step = PATH_CLOSING
+        
+        else:
+            path_step = PATH_IN
+        
+        if len(path) > 1 and path[1] == 'views' and path[-1] == 'pos':
+            # set box pos in one line
+            if path_step == PATH_OPENING:
+                final_str += line
+            
+            elif path_step == PATH_CLOSING:
+                final_str += strip
                 final_str += '\n'
-                comp_line = ''
+                
+            else:
+                final_str += strip
+                if line.endswith(','):
+                    final_str += ' '
+                
+        elif len(path) >= 6 and path[1] == 'portgroups':
+            # organize portgroups
+            if len(path) == 6:
+                if path_step == PATH_OPENING:
+                    final_str += line
+                    
+                elif path_step == PATH_CLOSING:
+                    final_str += strip
+                    final_str += '\n'
+                
+                else:
+                    # only concerns "above_metadatas"
+                    final_str += line[1:]
+            
+            elif len(path) == 7 and path[-1] == 'port_names':
+                if path_step == PATH_OPENING:
+                    final_str += strip
+                
+                elif path_step == PATH_CLOSING:
+                    final_str += strip
+                    if line.endswith(','):
+                        final_str += '\n'
+                
+                else:
+                    final_str += strip
+                    if line.endswith(','):
+                        final_str += '\n'
+                        for i in range(26):
+                            final_str += ' '
+            
+            else:
+                final_str += strip
+                if strip.endswith(','):
+                    final_str += ' '
+            
         else:
             final_str += line
             final_str += '\n'
-            
+
+        if path_step == PATH_CLOSING:
+            path = path[:-1]
+
     return final_str
 
-
-
-        
 
 class PortMode(IntFlag):
     NULL = 0x00
