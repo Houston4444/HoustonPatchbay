5099dfbdfd760aa17c7e6768603412665ec8f0a3
Houston4444 <picotmathieu@gmail.com>
2026-04-22 09:07:56 +0200

    change icon finder algo, now based on a desktop files parsing at startup

--- source/icon_finder/__init__.py
+++ source/icon_finder/__init__.py
@@ -1,62 +1,70 @@
 import logging
+import threading
 import xdg
 
 
 _logger = logging.getLogger(__name__)
 _all_icons = dict[str, str]()
+_parse_thread: threading.Thread
+
 
 def _kind_match(input: str) -> str:
     return input.replace('_', ' ').replace('-', ' ').lower()
 
-def _parse():
+def _parse_desktop_files():
+    parsed_desktops = set[str]()
+    
     for path in xdg.xdg_data_dirs():
-        hicolor = path / 'icons' / 'hicolor'
-        if not hicolor.is_dir():
+        apps_dir = path / 'applications'
+        if not apps_dir.is_dir():
             continue
-        
-        for sizes_dir in hicolor.iterdir():
-            if not sizes_dir.is_dir():
+        for desktop_file in apps_dir.iterdir():
+            if desktop_file.name in parsed_desktops:
                 continue
             
-            apps_dir = sizes_dir / 'apps'
-            if not apps_dir.is_dir():
+            if not desktop_file.name.endswith('.desktop'):
                 continue
             
-            for image_file in apps_dir.iterdir():
-                if not image_file.is_file():
-                    continue
-                icon_name = image_file.name.rpartition('.')[0]
-                icon_ref = _kind_match(icon_name.rpartition('.')[2])
-                if icon_ref in _all_icons:
-                    continue
-                
-                _all_icons[icon_ref] = icon_name
-                
-    for path in xdg.xdg_data_dirs():
-        pixmaps = path / 'pixmaps'
-        if not pixmaps.is_dir():
-            continue
-        
-        for image_file in pixmaps.iterdir():
-            if not image_file.is_file():
+            parsed_desktops.add(desktop_file.name)
+            
+            try:
+                with open(desktop_file, 'r') as f:
+                    contents = f.read(65535)
+            except:
                 continue
-            icon_name = image_file.name.rpartition('.')[0]
-            icon_ref = _kind_match(icon_name)
-            if icon_ref in _all_icons:
-                continue
-            _all_icons[icon_ref] = icon_name
+            
+            lines = contents.splitlines()
+            app_name, icon_name = '', ''
+            
+            for line in lines:
+                if line.startswith('Name='):
+                    app_name = line.partition('=')[2]
+                elif line.startswith('Icon='):
+                    icon_name = line.partition('=')[2]
+                
+                if app_name and icon_name:
+                    break
+            
+            if app_name:
+                _all_icons[_kind_match(app_name)] = icon_name
+
+def _try_parse_desktop_files():
+    try:
+        _parse_desktop_files()
+    except Exception as e:
+        _logger.warning(
+            f'Failed to parse desktop files to find icons\n{str(e)}')
 
 def get_icon_name_for(app_name: str) -> str:
+    if _parse_thread.is_alive():
+        _parse_thread.join()
+    
     return _all_icons.get(_kind_match(app_name), '')
 
-def refresh():
-    _all_icons.clear()
-    _parse()
 
+# at module import, desktop files are parsed in another thread.
+# The complete parse may have a duration of > 100ms at first startup,
+# then > 50ms, but it really depends on the hard drive speed (SSD or HDD).
+_parse_thread = threading.Thread(target=_try_parse_desktop_files)
+_parse_thread.start()
 
-# at module import, icon dirs are parsed and _all_icons is written
-try:
-    _parse()
-except Exception as e:
-    _logger.warning(f'Failed to parse icon dirs\n{str(e)}')
-    
