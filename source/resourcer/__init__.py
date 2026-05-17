3dc0f7fb774dd41da7f68c7d8a071220c6d15dcb
Houston4444 <picotmathieu@gmail.com>
2026-05-17 15:58:18 +0200

    manage multiple resources dirs and main_icon

--- source/resourcer/__init__.py
+++ source/resourcer/__init__.py
@@ -1,28 +1,38 @@
 import logging
 from pathlib import Path
 
-from qtpy.QtGui import QPixmap, QIcon, QFontDatabase
+from qtpy.QtGui import QPixmap, QIcon, QFontDatabase, QGuiApplication
 
 
 _logger = logging.getLogger(__name__)
 _dark_pixmaps_cache = dict[str, QPixmap]()
 _light_pixmaps_cache = dict[str, QPixmap]()
-resources_path = Path(__file__).parents[2] / 'resources'
-scalables = resources_path / 'scalables'
+resources_paths = [Path(__file__).parents[2] / 'resources']
 
 
 def _get_path(rel_path: str, dark=True) -> Path:
-    if dark:
-        img_path = scalables / 'dark' / rel_path
-    else:
-        img_path = scalables / 'light' / rel_path
-
-    if not img_path.is_file():
-        if dark:
-            img_path = scalables / 'light' / rel_path
-        else:
-            img_path = scalables / 'dark' / rel_path
+    img_path = Path()
     
+    for resources_path_ in resources_paths:
+        scalables = resources_path_ / 'scalables'
+        if not scalables.is_dir():
+            continue
+
+        if dark:
+            img_path = scalables / 'dark' / rel_path
+        else:
+            img_path = scalables / 'light' / rel_path
+
+        if not img_path.is_file():
+            if dark:
+                img_path = scalables / 'light' / rel_path
+            else:
+                img_path = scalables / 'dark' / rel_path
+        
+        if img_path.is_file():
+            return img_path
+    
+    _logger.warning(f'No scalable found: {rel_path}')
     return img_path
 
 def pixmap(rel_path: str, dark=True) -> QPixmap:
@@ -48,11 +58,24 @@ def pixmap(rel_path: str, dark=True) -> QPixmap:
 def icon(rel_path: str, dark=True) -> QIcon:
     return QIcon(pixmap(rel_path, dark=dark))
 
+def main_icon() -> QIcon:
+    app_name = QGuiApplication.applicationName().lower()
+    for resources_path_ in resources_paths:
+        main_icon_svg = \
+            resources_path_ / 'main_icon' / 'scalable' / f'{app_name}.svg'
+        if main_icon_svg.is_file():
+            return QIcon(str(main_icon_svg))
+    return QIcon()
+
 def icon_path(rel_path: str, dark=True) -> str:
     return str(_get_path(rel_path, dark=dark))
 
 def install_fonts():
-    fonts_dir = resources_path / 'fonts'
-    for font_file in fonts_dir.iterdir():
-        if font_file.is_file() and font_file.name.endswith('.ttf'):
-            QFontDatabase.addApplicationFont(str(font_file))
+    for resources_path_ in resources_paths:
+        fonts_dir = resources_path_ / 'fonts'
+        if not fonts_dir.is_dir():
+            continue
+        
+        for font_file in fonts_dir.iterdir():
+            if font_file.is_file() and font_file.name.endswith('.ttf'):
+                QFontDatabase.addApplicationFont(str(font_file))
